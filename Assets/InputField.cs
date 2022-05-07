d8c97011ca5a057c2812f068ebda4cb51b8aacf3
ericburger <eric.burger@comcast.net>
2022-05-07 16:34:57 -0500

    Update input script

--- Assets/InputField.cs
+++ Assets/InputField.cs
@@ -6,14 +6,19 @@ public class InputField : MonoBehaviour
 {
     public string placeholder = "Test placeholder";
     public GameObject mug;
-    GlideBehavior mugGliding;
+    public GameObject cabinet;
+    public Camera camera;
+
+    GlideBehavior cameraGliding;
 
     private Dictionary<string, string[]> inputStringsMap = new Dictionary<string, string[]>
     {
-        {"Mug", new string[]{"coffee", "mug", "cup"} }
+        {"Mug", new string[]{"coffee", "mug", "cup", "brew", "cafe"} },
+        {"Cabinet", new string[]{"cabinet, shelves"}}
     };
     private Dictionary<string, GameObject> gameObjectMap;
     private Dictionary<string, Vector3> transformMap;
+    private Dictionary<string, Vector3> cameraPositions;
     private Dictionary<string, GlideBehavior> glideMap;
     private string input;
     // Start is called before the first frame update
@@ -21,7 +26,8 @@ public class InputField : MonoBehaviour
     {
         gameObjectMap = new Dictionary<string, GameObject>
     {
-        {"Mug", mug }
+        {"Mug", mug },
+        {"Cabinet", cabinet}
     };
 
         glideMap = new Dictionary<string, GlideBehavior>
@@ -31,8 +37,18 @@ public class InputField : MonoBehaviour
         
         transformMap = new Dictionary<string, Vector3>
         {
-            {"Mug",  new Vector3(-0.5f, 1.2f, -7.5f)}
+            {"Mug",  new Vector3(0.0f, 1.6f, -9.5f)}
+            // Vector3(Y, ???, X)
+            //new Vector3(-0.7f, 1.6f, -9.3f)
         };
+        
+        cameraPositions = new Dictionary<string, Vector3>
+        {
+            {"Cabinet",  new Vector3(0.0f, 1.6f, -9.5f)}
+            // Vector3(Y, ???, X)
+            //new Vector3(-0.7f, 1.6f, -9.3f)
+        };
+        cameraGliding = camera.GetComponent<GlideBehavior>();
     }
 
     // Update is called once per frame
@@ -45,16 +61,25 @@ public class InputField : MonoBehaviour
     {
         foreach (KeyValuePair<string, string[]> entry in inputStringsMap)
         {
-            // do something with entry.Value or entry.Key
+            // Check if there is a camera position associated with the input.
             string[] inputWords = s.ToLower().Split(' ');
             HashSet<string> inputSet = new HashSet<string>(inputWords);
             HashSet<string> mapSet = new HashSet<string>(entry.Value);
-            
+
             if (inputSet.IsSubsetOf(mapSet))
             {
                 Vector3 transformVector = transformMap[entry.Key];
-                GlideBehavior glide = glideMap[entry.Key];
-                glide.SetDestination(transformVector);
+                if (glideMap.ContainsKey(entry.Key))
+                {
+                    GlideBehavior glide = glideMap[entry.Key];
+                    glide.SetDestination(transformVector);
+
+                }
+                if (cameraPositions.ContainsKey(entry.Key))
+                {
+                    Vector3 cameraPosition = cameraPositions[entry.Key];
+                    cameraGliding.SetDestination(cameraPosition);
+                }
             }
         }
     }
