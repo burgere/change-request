3b91c94fbe87c788e5ab83f1adcf4e7fb3b379a3
Matt Schwartzwalder <matthew.schwartzwalder@intpark.com>
2022-05-07 16:33:06 -0500

    MS - camera script

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -5,132 +5,31 @@ using UnityEngine.EventSystems;
 
 public class CameraScript : MonoBehaviour
 {
-  public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
-  public RotationAxes axes = RotationAxes.MouseXAndY;
-  public float sensitivityX = 2F;
-  public float sensitivityY = 2F;
-  public float minimumX = -360F;
-  public float maximumX = 360F;
-  public float minimumY = -90F;
-  public float maximumY = 90F;
-  float rotationY = -60F;
+    Vector3 relativePosition;
+    Quaternion targetRotation;
+    public Transform target;
+    public float speed = 0.01f;
 
-  // For camera movement
-  float CameraPanningSpeed = 10.0f;
+    bool rotating = false;
 
+    float rotationTime; // when rotationTime = 1 then we have rotated fully to target
+    void Update()
+    {
 
-  void Update()
-  {
-      MouseInput();
-  }
-
-  void MouseInput()
-  {
-      if (EventSystem.current.IsPointerOverGameObject())
-      {
-          return;
-      }
-
-      if (Input.GetMouseButton(0))
-      {
-      }
-      else if (Input.GetMouseButton(1))
-      {
-          MouseRightClick();
-      }
-      else if (Input.GetMouseButton(2))
-      {
-          MouseMiddleButtonClicked();
-      }
-      else if (Input.GetMouseButtonUp(1))
-      {
-          ShowAndUnlockCursor();
-      }
-      else if (Input.GetMouseButtonUp(2))
-      {
-          ShowAndUnlockCursor();
-      }
-      else
-      {
-          MouseWheeling();
-      }
-  }
-
-  void ShowAndUnlockCursor()
-  {
-      Cursor.lockState = CursorLockMode.None;
-      Cursor.visible = true;
-  }
-
-  void HideAndLockCursor()
-  {
-      Cursor.lockState = CursorLockMode.Locked;
-      Cursor.visible = false;
-  }
-
-  void MouseMiddleButtonClicked()
-  {
-      HideAndLockCursor();
-      Vector3 NewPosition = new Vector3(Input.GetAxis("Mouse X"), 0, Input.GetAxis("Mouse Y"));
-      Vector3 pos = transform.position;
-      if (NewPosition.x > 0.0f)
-      {
-          pos += transform.right;
-      }
-      else if (NewPosition.x < 0.0f)
-      {
-          pos -= transform.right;
-      }
-      if (NewPosition.z > 0.0f)
-      {
-          pos += transform.forward;
-      }
-      if (NewPosition.z < 0.0f)
-      {
-          pos -= transform.forward;
-      }
-      pos.y = transform.position.y;
-      transform.position = pos;
-  }
-
-  void MouseRightClick()
-  {
-      HideAndLockCursor();
-      if (axes == RotationAxes.MouseXAndY)
-      {
-          float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
-
-          rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-          rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
-
-          transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
-      }
-      else if (axes == RotationAxes.MouseX)
-      {
-          transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
-      }
-      else
-      {
-          rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-          rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
-
-          transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
-      }
-  }
-
-  void MouseWheeling()
-  {
-      Vector3 pos = transform.position;
-      if (Input.GetAxis("Mouse ScrollWheel") < 0)
-      {
-          pos = pos - transform.forward;
-          transform.position = pos;
-      }
-      if (Input.GetAxis("Mouse ScrollWheel") > 0)
-      {
-          pos = pos + transform.forward;
-          transform.position = pos;
-      }
-  }
-
+        if(Input.GetKeyDown(KeyCode.F)){
+            relativePosition = target.position - transform.position;
+            targetRotation = Quaternion.LookRotation(relativePosition);
+            rotating = true;
+            rotationTime = 0;
+        }
+        if (rotating)
+        {
+          rotationTime += Time.deltaTime * speed;
+          transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationTime * speed);
+        }
+        if (rotationTime > 1)
+        {
+          rotating = false;
+        }
+    }
 }
